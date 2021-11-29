a7961b48f18590d98f9e1c118726d83b77d8a5a3
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2021-11-29 12:23:15 -0500

    v-if to v-show
    trying to fix appendChild error

--- .vuepress/theme/global-components/SchemaFull.vue
+++ .vuepress/theme/global-components/SchemaFull.vue
@@ -1,16 +1,16 @@
 <template>
     <div id="hierarchy-container">
           <ul>
-            <!-- Beginning of description section --> 
-            <li> <a href="./core.html">core</a>  </li>
-              <!-- Beginning of each property in description list --> 
+            <!-- Beginning of description section -->
+            <li><a href="./core.html">core</a></li>
+              <!-- Beginning of each property in description list -->
               <ul v-for="item, index in this.core" :key="index">
                 <li>
                   <a :href="index + '.html'" >{{index}}</a>
                 </li>
               </ul>
-            <!-- Beginning of Data Endpoint section --> 
-            <li v-if="this.access.items"><a href="./access.html"> access </a></li>
+            <!-- Beginning of Data Endpoint section -->
+            <li v-show="this.access.items"><a href="./access.html"> access </a></li>
                 <ul>
                   <li><a href="$id.html" >$id</a></li>
                   <li><a href="accessURL.html" >accessURL</a></li>
@@ -18,28 +18,28 @@
                   <li><a href="source.html" >source</a></li>
                   <li><a href="format.html" >format</a></li>
                 </ul>
-            <!-- Beginning of Filter Tagging section --> 
+            <!-- Beginning of Filter Tagging section -->
             <li><a href="./tags.html"> tags </a></li>
                 <ul v-for="item, index in this.tags" :key="index">
                   <li><a :href="index + '.html'" >{{index}}</a></li>
                 </ul>
-            <!-- Beginning of People Lifecycle section --> 
+            <!-- Beginning of People Lifecycle section -->
             <li><a href="./lifecycle.html"> lifecycle </a></li>
                 <ul v-for="item, index in this.lifecycle" :key="index">
                   <li><a :href="index + '.html'" >{{index}}</a></li>
                 </ul>
-            <!-- Beginning of Documentation Health section --> 
+            <!-- Beginning of Documentation Health section -->
             <li><a href="./considerations.html"> considerations </a></li>
                 <ul v-for="item, index in this.considerations" :key="index">
                   <li><a :href="index + '.html'" >{{index}}</a></li>
                 </ul>
-            <!-- Beginning of Related Resources section --> 
+            <!-- Beginning of Related Resources section -->
             <li><a href="./resources.html"> resources </a></li>
                 <ul v-for="item, index in this.resources" :key="index">
                   <li><a :href="index + '.html'" >{{index}}</a></li>
                 </ul>
           </ul>
-          <!-- end of full hierarchy list --> 
+          <!-- end of full hierarchy list -->
     </div>
 </template>
 
@@ -87,4 +87,3 @@ export default {
     }
 }
 </script>
-
